d18e951e788fdcb811a1f2df30e29414967df0fb
Michael <michael@falsebehavinganimals.com>
2014-12-31 13:09:30 +0100

    file upload to s3 linked up

---
+++ client/templates/app/s3Upload/upload.js
@@ -0,0 +1,14 @@
+Template.s3_upload.events({
+    "click button.upload": function(){
+        var files = $("input.file_bag")[0].files
+        S3.upload(files,"/subfolder",function(e,r){
+            console.log(r);
+        });
+    }
+})
+
+Template.s3_upload.helpers({
+    "files": function(){
+        return S3.collection.find();
+    }
+})
---
+++ server/amazon.js
@@ -0,0 +1,5 @@
+S3.config = {
+    key: 'AKIAJPQVJZNFXD3MF6LQ',
+    secret: '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',
+    bucket: 'mycustom'
+};
